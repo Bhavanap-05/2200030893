53db6589e18371b1b540683483d15546ec55e51f
Bhavanap-05 <116939004+Bhavanap-05@users.noreply.github.com>
2024-12-07 09:54:26 +0530

    Add files via upload

---
+++ OrderController.java
@@ -0,0 +1,19 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order savedOrder = orderService.saveOrder(order);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+}
